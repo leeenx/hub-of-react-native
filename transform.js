6927d554a680adcdcb8c54d9516711184c8c8df8
leeenx <leeenx@users.noreply.github.com>
2019-05-30 16:05:38 +0800

    支持 css3 的写法

--- transform.js
+++ transform.js
@@ -46,19 +46,20 @@ function scaleY (y) {
 function scaleZ (z) {
   return scale3d(1, 1, z)
 }
-function rotate3d (x, y, z, deg) {
-  const degZ = -atan(y / x) * 180 / PI
-  const degY = -atan(sqrt(x * x + y * y) / z) * 180 / PI
+function rotate3d (x, y, z, angle) {
+  const rad = toRad(angle)
+  const radZ = -atan(y / x)
+  const radY = -atan(sqrt(x * x + y * y) / z)
   return matrix3dBy(
-    rotateZ(-degZ),
-    rotateY(-degY),
-    rotateZ(deg),
-    rotateY(degY),
-    rotateZ(degZ)
+    rotateZ(-radZ),
+    rotateY(-radY),
+    rotateZ(rad),
+    rotateY(radY),
+    rotateZ(radZ)
   )
 }
-function rotateX (deg) {
-  const rad = degToRad(deg)
+function rotateX (angle) {
+  const rad = toRad(angle)
   return [
     1, 0, 0, 0,
     0, cos(rad), -sin(rad), 0,
@@ -66,8 +67,8 @@ function rotateX (deg) {
     0, 0, 0, 1
   ]
 }
-function rotateY (deg) {
-  const rad = degToRad(deg)
+function rotateY (angle) {
+  const rad = toRad(angle)
   return [
     cos(rad), 0, sin(rad), 0,
     0, 1, 0, 0,
@@ -75,8 +76,8 @@ function rotateY (deg) {
     0, 0, 0, 1
   ]
 }
-function rotateZ (deg) {
-  const rad = degToRad(deg)
+function rotateZ (angle) {
+  const rad = toRad(angle)
   return [
     cos(rad), -sin(rad), 0, 0,
     sin(rad), cos(rad), 0, 0,
@@ -91,16 +92,15 @@ function translate (x, y = x) {
 function scale (x, y = x) {
   return scale3d(x, y, 1)
 }
-function rotate (deg) {
-  return rotateZ(deg)
+function rotate (rad) {
+  return rotateZ(rad)
 }
 function skew (x) {
   return skewXY(x, 0)
 }
 function skewXY (x, y = x) {
-  const radX = degToRad(x)
-  const radY = degToRad(y)
-  console.log(radX, radY)
+  const radX = toRad(x)
+  const radY = toRad(y)
   // 2d扩展为 matrix3d
   return [
     1, tan(radX), 0, 0,
@@ -115,15 +115,18 @@ function skewX (x) {
 function skewY (y) {
   return skewXY(0, y)
 }
+// 2d matrix 转 3d
+function matrix2dToMatrix3d (a, b, c, d, e, f) {
+  return createCSS3Matrix3d(a, b, 0, c, d, e, 0, f, 0, 0, 1, 0)
+}
 function createCSS3Matrix3d (a, b, c, d, e, f, g, h, i, j, k, l) {
   // css3 的 matrix3d
-  const css3Matrix3d = [
+  return [
     a, e, i, 0,
     b, f, j, 0,
     c, g, k, 0,
     d, h, l, 1
   ]
-  return `matrix3d(${css3Matrix3d.join(',')})`
 }
 // 矩阵相乘
 function matrix3dBy (...matrixes) {
@@ -178,38 +181,129 @@ function fetchMatrixCol (matrix) {
   }
 }
 // transform-origin
-function setTransformOrigin (x = 0, y = x, z = 0) {
-  return matrix => {
-    const [
-      a, b, c, d,
-      e, f, g, h,
-      i, j, k, l
-    ] = matrix
-    return [
-      a, b, c, x + d - a * x - b * y - c * z,
-      e, f, g, y + h - e * x - f * y - g * z,
-      i, j, k, z + l - i * x - j * y - k * z,
-      0, 0, 0, 1
-    ]
+function setTransformOrigin (matrix3d, [ x = 0, y = x, z = 0 ]) {
+  const [
+    a, b, c, d,
+    e, f, g, h,
+    i, j, k, l
+  ] = matrix3d
+  return [
+    a, b, c, x + d - a * x - b * y - c * z,
+    e, f, g, y + h - e * x - f * y - g * z,
+    i, j, k, z + l - i * x - j * y - k * z,
+    0, 0, 0, 1
+  ]
+}
+// 合并matrix3d
+function mergeMatrix3ds (matrix3ds, origin = [x = 0, y = 0, z = 0]) {
+  // 设置 transform-origin
+  matrix3ds.forEach((matrix3d, index) => {
+    matrix3ds[index] = setTransformOrigin(matrix3d, origin)
+  })
+  // 返回合成值
+  return matrix3dBy(...matrix3ds)
+}
+// 度数转弧度
+function degToRad (deg) {
+  return deg / 180 * PI
+}
+// 梯度转弧度
+function gradToRad (grad) {
+  return grad / 200 * PI
+}
+// 圈度转弧度
+function turnToRad (turn) {
+  return turn * 2 * PI
+}
+// 转为弧度
+function toRad (angle) {
+  const value = parseFloat(angle)
+  const unit = angle.toLowerCase().replace(`${value}`, '')
+  switch (unit) {
+    case 'deg':
+      return degToRad(value)
+    case 'grad':
+      return gradToRad(value)
+    case 'turn':
+      return turnToRad(value)
+    case 'rad':
+    default:
+      return value
   }
 }
-// 这个方法应该不会被调用
-function genCSS3Transform (...transforms) {
-  let setOrigin = matrix => matrix
-  const matrix3ds = transforms.filter(
-    matrix3d => {
-      if (typeof matrix3d === 'function') {
-        setOrigin = matrix3d
-        return false
-      }
-      return true
-    }
-  ).map(matrix3d =>
-    createCSS3Matrix3d(...setOrigin(matrix3d))
-  ).join(' ')
-  return `transform: ${matrix3ds}`
+
+function transformFunToMatrix3d (name, transformParams) {
+  const transformFunction = transformFunctions[name]
+  if (transformFunction) {
+    return transformFunction(...transformParams)
+  } else {
+    // 没有对应的 transform-function
+    return matrix(1, 0, 0, 0, 1, 0)
+  }
 }
-function degToRad (deg) {
-  // 度数转弧度
-  return deg / 180 * PI
+
+// 方法集
+export const transformFunctions = {
+  translate,
+  translate3d,
+  translateX,
+  translateY,
+  translateZ,
+  scale,
+  scale3d,
+  scaleX,
+  scaleY,
+  scaleZ,
+  rotate,
+  rotate3d,
+  rotateX,
+  rotateY,
+  rotateZ,
+  skew,
+  skewX,
+  skewY,
+  skewXY,
+  matrix: matrix2dToMatrix3d,
+  matrix3d: createCSS3Matrix3d,
+  transformFunToMatrix3d,
+  degToRad,
+  toRad
+}
+
+// 缓存
+const transformCache = {}
+
+// 将 transformStr 转成 React Native 的 matrix3d
+export default function transform (pattern, originStr) {
+  if (!transformCache[pattern]) {
+    const transformStrList = pattern
+      .replace(/^\s+|\s+$/g, '')
+      .replace(/\,\s+/, ',')
+      .replace(/\s+\(/, ')')
+      .split(/\s+/)
+    const matrix3ds = transformStrList.map(transformStr => {
+      const index1 = transformStr.indexOf('(')
+      const index2 = transformStr.indexOf(')')
+      const transformFunName = transformStr.substring(0, index1)
+      const [
+        x,
+        y = x,
+        z = '0',
+        angle = '0'
+      ] = transformStr.substring(index1 + 1, index2).split(',')
+      return transformFunToMatrix3d(transformFunName, [x, y, z, angle])
+    })
+    // origin
+    const origin = (
+      originStr !== null ?
+        originStr.split(/\s+/).map(value => Number(value)) :
+        [0, 0, 0]
+    )
+    // 合并 matrix3ds
+    const matrix = createCSS3Matrix3d(
+      ...mergeMatrix3ds(matrix3ds, origin)
+    )
+    transformCache[pattern] = [{ matrix }]
+  }
+  return transformCache[pattern]
 }
